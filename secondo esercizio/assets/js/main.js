4a50c20f63ed97f1e29b0d6f964538d2911d38a7
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-03-30 23:22:01 +0200

    Esercizo 2 - Terminato

--- secondo esercizio/assets/js/main.js
+++ secondo esercizio/assets/js/main.js
@@ -24,7 +24,7 @@ function funPariDispari(num1, num2){
 }
 
 //assegno ad una costante il pulsante
-let button = document.querySelector('button') 
+let button = document.querySelector('#gioca') 
 
 //assegno al click sul pulsante "gioca" l'avvio di una nuova funzione che acquisisce i dati inseriti dall'utente, assegna un numero randomico alla cpu tramite la funzione scritta sopra, effettua la somma e assegna il vincitore
 button.addEventListener('click', function(){
@@ -38,18 +38,37 @@ button.addEventListener('click', function(){
     //assegno ad una variabile la scelta del numero da parte dell'utente tramite la select
     let numeroUtente = parseInt(document.querySelector("#sceltaNumero").value)
     console.log(numeroUtente)
+    document.querySelector('#tu').innerHTML += `<h3 class="text-center">${numeroUtente}</h3>`
 
     //assegno ad una variabile il numero della cpu
     const numeroCpu = numeroRandomico(1,5)
     console.log(numeroCpu)
+    document.querySelector('#cpu').innerHTML += `<h3 class="text-center">${numeroCpu}</h3>`
 
     //assegno la somma dei due numeri ad una variabile
     let somma = numeroUtente + numeroCpu
     console.log (somma)
 
+    console.log(funPariDispari(numeroUtente, numeroCpu))
+
     if(pari == funPariDispari(numeroUtente, numeroCpu)){
         console.log('hai vinto!')
-    } else {
+        document.querySelector('#result').innerHTML += `
+        <p>La somma è ${somma}</p>
+        <h3 class="text-success">hai vinto!!!</h3>
+        `
+    } else if (pari != funPariDispari(numeroUtente, numeroCpu)) {
         console.log('non hai vinto!')
-    }
+        document.querySelector('#result').innerHTML += `
+        <p>La somma è ${somma}</p>
+        <h3 class="text-danger">hai perso</h3>
+        `
+    } 
+})
+
+// prendo il pulsante refresh e lo associo ad una variabile
+const buttonRefresh = document.querySelector('#refresh')
+// associo al click del pulsante Refresh la funzione che effettua il refresh della pagina
+buttonRefresh.addEventListener('click', function(){
+    window.location.reload();
 })
