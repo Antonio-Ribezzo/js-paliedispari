22f5c2bd74dd4ae873affbd26ffcc61364e0cfcf
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-03-30 13:37:07 +0200

    Upload Files

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+/*
+Primo esercizio:
+    - Palidroma
+        - Chiedere all’utente di inserire una parola. 
+        - Creare una funzione per capire se la parola inserita è palindroma.
+Secondo esercizio:
+    - Pari e Dispari
+        - L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. 
+        - Genero un numero random (sempre da 1 a 5) per il computer (usando una funzione). 
+        - Sommo i due numeri. 
+        - Stabilisco se la somma dei due numeri è pari o dispari (usando una funzione).
+        - Dichiaro chi ha vinto.
+*/
---
+++ readme.md
@@ -0,0 +1,5 @@
+## First exercise 
+    Chiedere all’utente di inserire una parola e creare una funzione per capire se la parola inserita è palindroma.
+#
+## Second exercise
+    L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Genero un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommo i due numeri. Stabilisco se la somma dei due numeri è pari o dispari (usando una funzione).Dichiaro chi ha vinto.
